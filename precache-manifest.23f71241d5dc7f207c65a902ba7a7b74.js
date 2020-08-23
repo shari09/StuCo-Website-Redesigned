11ea5a7909d085ec5e6b124c16e7a1fa82cb412c
shari09 <shari09cxxcr@gmail.com>
2020-08-23 19:16:22 -0400

    Updates

---
+++ precache-manifest.23f71241d5dc7f207c65a902ba7a7b74.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c882b223477d253ab475dd0f5023010",
+    "url": "/StuCo-Website-Redesigned/index.html"
+  },
+  {
+    "revision": "e4131b69e52428ea0844",
+    "url": "/StuCo-Website-Redesigned/static/css/main.6fdb4904.chunk.css"
+  },
+  {
+    "revision": "e4e13f62cbd02482e056",
+    "url": "/StuCo-Website-Redesigned/static/js/2.04dc9758.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StuCo-Website-Redesigned/static/js/2.04dc9758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4131b69e52428ea0844",
+    "url": "/StuCo-Website-Redesigned/static/js/main.c185dd26.chunk.js"
+  },
+  {
+    "revision": "3ba4872cef8817afb154",
+    "url": "/StuCo-Website-Redesigned/static/js/runtime-main.25754182.js"
+  },
+  {
+    "revision": "12fcb1cf3c4bf8b72db5435ea10a4b61",
+    "url": "/StuCo-Website-Redesigned/static/media/BottomPageDecor.12fcb1cf.svg"
+  },
+  {
+    "revision": "fdd1f9d6b6780f2af93bf9e0c3f97013",
+    "url": "/StuCo-Website-Redesigned/static/media/clubBackground.fdd1f9d6.png"
+  },
+  {
+    "revision": "244fe6efecb85c1fff91c5d092bf73a3",
+    "url": "/StuCo-Website-Redesigned/static/media/logo.244fe6ef.svg"
+  },
+  {
+    "revision": "4c8ab52f65ddd6ae4b19f803ffd8a17b",
+    "url": "/StuCo-Website-Redesigned/static/media/speech bubble.4c8ab52f.svg"
+  },
+  {
+    "revision": "b2f1e424291d017f64edbff7141c419f",
+    "url": "/StuCo-Website-Redesigned/static/media/speechBubbleMobile.b2f1e424.svg"
+  }
+]);
